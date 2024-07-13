88b83b770759bca5f8d39a697a57771b1957a5c1
Samir DERKAOUI <126152143+Kaiser016X@users.noreply.github.com>
2024-07-13 15:37:25 +0200

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -2,3 +2,15 @@ variable "vpc_id" {
   description = "VPC ID"
   type        = string
 }
+
+variable "sg_web_description" {
+  description = "SG WEB"
+  type        = string
+  default     = "Ouvre port 80 et 443"
+}
+
+variable "sg_ssh_description" {
+  description = "SG SSH"
+  type        = string
+  default     = "Ouvre port 22"
+}
