d316531f5e0e215ee04d728e4dd1ebe63d662c11
Samir <contact@sderkaoui.com>
2024-07-29 16:28:43 +0200

    aaa

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,19 @@
-output "sg_ssh_o" {
-  value = aws_security_group.sg_ssh.id
+output "sg_web_description" {
+  value = aws_security_group.sg_web_description.id
 }
 
-output "sg_web_o" {
-  value = aws_security_group.sg_web.id
+output "sg_externe_ssh" {
+  value = aws_security_group.sg_externe_ssh.id
+}
+
+output "sg_interne_ssh" {
+  value = aws_security_group.sg_interne_ssh.id
+}
+
+output "sg_openvpn" {
+  value = aws_security_group.sg_openvpn.id
+}
+
+output "sg_openall_ports" {
+  value = aws_security_group.sg_openall_ports.id
 }
