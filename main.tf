be0ca4e926d1155bf4b2cdd9ec715e415106fd41
Samir DERKAOUI <126152143+Kaiser016X@users.noreply.github.com>
2024-08-01 15:17:26 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -174,6 +174,19 @@ resource "aws_security_group" "sg_openall_ports" {
     ]
   }
 
+  ingress {
+    description = "All ICMP"
+    from_port   = 1
+    to_port     = 65535
+    protocol    = "icmp"
+    cidr_blocks = [
+      "10.0.1.0/24",
+      "10.0.2.0/24",
+      "10.0.101.0/24",
+      "10.0.102.0/24"
+    ]
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
